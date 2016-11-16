47bf6fa4756e6a5b2591e82492c051477093313e
myer0154 <myer0154@umn.edu>
2016-11-15 18:50:38 -0600

    Create prototype for corr.R

---
+++ Assignment 1 - Week 2/corr.R
@@ -0,0 +1,12 @@
+## 'directory' is a character vector of length 1 indicating the location of the
+## CSV files
+
+## 'threshold' is a numeric vector of length 1 indicating the number of 
+## completely observed observations (on all variables) required to computer the
+## correlation between nitrate and sulfate; the default is zero
+
+## Return a numeric vector of correlations
+
+corr <- function(directory, threshold = 0) {
+	
+}
